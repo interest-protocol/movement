9c7ee0e9c00e70621b4e1939ae0ccfeeb8eb0da7
git-marcopitra <marcomorete73@hotmail.com>
2024-06-01 22:00:14 +0100

    :sparkles: feat: add error boundary

---
+++ components/error-boundary/index.tsx
@@ -0,0 +1,30 @@
+import { Component, PropsWithChildren } from 'react';
+
+import Error from '@/views/error';
+
+class ErrorBoundary extends Component<PropsWithChildren> {
+  state: {
+    hasError: boolean;
+  };
+
+  constructor(props: PropsWithChildren) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: any, errorInfo: any) {
+    console.log({ error, errorInfo });
+  }
+
+  render() {
+    if (this.state.hasError) return <Error />;
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
